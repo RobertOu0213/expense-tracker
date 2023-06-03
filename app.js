156c6de65fea7b027eb45f68ba9e256d8bce33fa
RobertOu0213 <starshining12345@gmail.com>
2023-06-03 18:42:33 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -2,20 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const exphbs = require("express-handlebars");
+require("./config/mongoose");
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
-
-const mongoose = require("mongoose");
-require("dotenv").config();
-mongoose.connect(process.env.MONGODB_URI);
-const db = mongoose.connection;
-db.on("error", () => {
-  console.log("mongodb error");
-});
-db.once("open", () => {
-  console.log("mongodb connected");
-});
 
 app.get("/", (req, res) => {
   res.render("index");

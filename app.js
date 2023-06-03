e37d8d0d46bba413022daaff762413bb73f64cbc
RobertOu0213 <starshining12345@gmail.com>
2023-06-03 22:26:08 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const exphbs = require("express-handlebars");
+const Record = require("./models/record");
 require("./config/mongoose");
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  Record.find()
+    .lean()
+    .then((records) => res.render("index", { records }))
+    .catch((error) => console.log(error));
 });
 
 app.listen(PORT, () => {

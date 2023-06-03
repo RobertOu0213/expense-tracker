56fec4271d58000273e59b0d7ff80d4d76d80c8c
RobertOu0213 <starshining12345@gmail.com>
2023-06-03 23:51:00 +0800

    feat: add create new page function

--- app.js
+++ app.js
@@ -8,10 +8,24 @@ require("./config/mongoose");
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
 
+//body parser
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   Record.find()
     .lean()
     .then((records) => res.render("index", { records }))
+    .catch((error) => console.log(error));
+});
+
+app.get("/new", (req, res) => {
+  res.render("new");
+});
+
+app.post("/records", (req, res) => {
+  const { name, date, category, amount } = req.body;
+  return Record.create({ name, date, category, amount })
+    .then(() => res.redirect("/"))
     .catch((error) => console.log(error));
 });
 

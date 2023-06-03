3da02512552e61f00b5afaed157ef9872accb680
RobertOu0213 <starshining12345@gmail.com>
2023-06-03 17:26:15 +0800

    feat: add mongoose & record model

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.connect(process.env.MONGODB_URI);
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("mongodb error");
+});
+db.once("open", () => {
+  console.log("mongodb connected");
+});
 
 app.get("/", (req, res) => {
   res.send("h1");

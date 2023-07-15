1717dadd80bffb7d43bdf0e71b6de189a5dd93d7
RobertOu0213 <starshining12345@gmail.com>
2023-07-15 13:52:49 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
 const PORT = 3000;
 const exphbs = require("express-handlebars");
 const Record = require("./models/record");
 const methodOverride = require("method-override");
 const routes = require("./routes/index");
+
 require("./config/mongoose");
 
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
+
+app.use(
+  session({
+    secret: "ThisIsAnApple",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //body parser
 app.use(express.urlencoded({ extended: true }));
